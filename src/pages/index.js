57976b2cea1a071456dff49a3d44d3b24d30b0f4
Cyberchain <cyberchain31@gmail.com>
2024-07-22 06:49:50 +0200

    one

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main>
+      <div>TEST</div>
+      <div>TEST</div>
+      <div>TEST</div>
+    </main>
+
+    
+  );
+}
